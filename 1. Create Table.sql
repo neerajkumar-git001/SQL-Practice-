e071e4cf7d8253e1c0e8956db2b24dc95ab55fa0
Neeraj Kumar Sahu <neerajsa2006@gmail.com>
2026-09-21 11:34:07 +0530

    Update 1. Create Table.sql

--- 1. Create Table.sql
+++ 1. Create Table.sql
@@ -1,3 +1,5 @@
+-- first way to create a table 
+
 create table School
 (
     student_id int primary key,
@@ -8,3 +10,17 @@ create table School
 );
 
 select * from School;
+
+
+-- Second way to crate a table 
+CREATE TABLE public."Teachers"
+(
+    "Teachers_id" integer,
+    "Teachers_name" character varying(50) NOT NULL,
+    "Age " integer NOT NULL,
+    "Course" character varying(30) NOT NULL,
+    PRIMARY KEY ("Teachers_id")
+);
+
+ALTER TABLE IF EXISTS public."Teachers"
+    OWNER to postgres;
